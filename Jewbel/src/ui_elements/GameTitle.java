33dbd0fae23f125b64d4c738c35878242ae82da2
Chrisdf <chrisfrantz96@gmail.com>
2015-01-28 20:44:40 -0600

    Improve game title logo

--- Jewbel/src/ui_elements/GameTitle.java
+++ Jewbel/src/ui_elements/GameTitle.java
@@ -5,33 +5,34 @@ import java.nio.file.Paths;
 import org.jsfml.graphics.Color;
 import org.jsfml.graphics.Drawable;
 import org.jsfml.graphics.FloatRect;
-import org.jsfml.graphics.Font;
 import org.jsfml.graphics.RenderStates;
 import org.jsfml.graphics.RenderTarget;
 import org.jsfml.graphics.RenderWindow;
+import org.jsfml.graphics.Sprite;
 import org.jsfml.graphics.Text;
+import org.jsfml.graphics.Texture;
 import org.jsfml.system.Vector2f;
 
 public class GameTitle implements Drawable {
 
-	private Font freeSans = new Font();
-
-	private Text gameTitle;
+	private Texture gameTitleImage;
+	
+	private Sprite gameTitle;
 
 	public GameTitle(RenderWindow renderWindow) {
 
+		gameTitleImage = new Texture();
+		
 		try
 		{
-			freeSans.loadFromFile(Paths.get("Resources/FreeSans.otf"));
+			gameTitleImage.loadFromFile(Paths.get("Resources/titleLogo.png"));
 		}
 		catch (Exception e)
 		{
 			e.printStackTrace();
 		}
 
-		gameTitle = new Text("BEJEWBLED", freeSans, 50);
-		gameTitle.setColor(Color.BLACK);
-		gameTitle.setStyle(Text.BOLD | Text.UNDERLINED);
+		gameTitle = new Sprite(gameTitleImage);
 
 		FloatRect gameTitleBounds = gameTitle.getLocalBounds();
 		gameTitle.setOrigin(gameTitleBounds.width / 2, gameTitleBounds.height / 3);
