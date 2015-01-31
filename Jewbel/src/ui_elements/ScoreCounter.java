758019e4adab6129c7e8cc9ecef7322014961e30
Chrisdf <chrisfrantz96@gmail.com>
2015-01-30 22:35:41 -0600

    Center score on text background

--- Jewbel/src/ui_elements/ScoreCounter.java
+++ Jewbel/src/ui_elements/ScoreCounter.java
@@ -4,6 +4,7 @@ import java.nio.file.Paths;
 
 import org.jsfml.graphics.Color;
 import org.jsfml.graphics.Drawable;
+import org.jsfml.graphics.FloatRect;
 import org.jsfml.graphics.Font;
 import org.jsfml.graphics.RectangleShape;
 import org.jsfml.graphics.RenderStates;
@@ -16,23 +17,29 @@ public class ScoreCounter implements Drawable {
 	
 	private int totalScore;
 	
-	private RectangleShape scoreBackground;
+	private RectangleShape rightScoreBackground;
 	
-	private Text displayedScore;
+	private Text rightDisplayedScore;
+	
+	private RectangleShape leftScoreBackground;
+	
+	private Text leftDisplayedScore;
 	
 	private Font freeSans;
 	
 	private RenderWindow window;
 	
-	private RectangleShape test;
-	
 	private float borderSize;
+	
+	private Vector2f leftPositioning;
+	
+	private Vector2f rightPositioning;
 	
 	public ScoreCounter(RenderWindow renderWindow){
 		
 		window = renderWindow;
 		freeSans = new Font();
-		scoreBackground = new RectangleShape();
+		rightScoreBackground = new RectangleShape();
 		
 		try
 		{
@@ -45,36 +52,47 @@ public class ScoreCounter implements Drawable {
 		
 		borderSize = 4f;
 		
-		Vector2f positioning = new Vector2f(window.getSize().x / 15, window.getSize().y / 8);
+		leftPositioning = new Vector2f(window.getSize().x / 15, window.getSize().y / 8);
+		rightPositioning = new Vector2f(window.getSize().x - window.getSize().x / 15, window.getSize().y / 8);
 		
-		displayedScore = new Text(totalScore + "", freeSans, 120);
-		displayedScore.setColor(Color.GREEN);
 		
-		Vector2f displaySize = new Vector2f(displayedScore.getLocalBounds().width, displayedScore.getLocalBounds().height);
+		rightDisplayedScore = new Text(totalScore + "", freeSans, 120);
+		FloatRect bounds = rightDisplayedScore.getLocalBounds();
+		rightDisplayedScore.setOrigin(bounds.left + bounds.width / 2.0f, bounds.top + bounds.height / 2.0f);
+		rightDisplayedScore.setColor(Color.BLACK);
+		
+		Vector2f displaySize = new Vector2f(rightDisplayedScore.getLocalBounds().width, rightDisplayedScore.getLocalBounds().height);
 		displaySize = Vector2f.mul(displaySize, 1.5f);
-		scoreBackground.setSize(displaySize);
-		scoreBackground.setFillColor(new Color(Color.CYAN, 150));
-		scoreBackground.setOutlineColor(Color.CYAN);
-		scoreBackground.setOutlineThickness(borderSize);
-		scoreBackground.setPosition(positioning);
-		
-		float centerXPosition = scoreBackground.getPosition().x + ((scoreBackground.getGlobalBounds().width - displayedScore.getLocalBounds().width) / 2) - borderSize;
-		float centerYPosition = scoreBackground.getPosition().y + ((scoreBackground.getGlobalBounds().height - displayedScore.getLocalBounds().height) / 2) - borderSize;
-		displayedScore.setPosition(centerXPosition, centerYPosition);
+		rightScoreBackground.setSize(displaySize);
+		rightScoreBackground.setFillColor(new Color(Color.CYAN, 150));
+		rightScoreBackground.setOutlineColor(Color.CYAN);
+		rightScoreBackground.setOutlineThickness(borderSize);
+		FloatRect backBounds = rightScoreBackground.getLocalBounds();
+		rightScoreBackground.setOrigin(backBounds.left + backBounds.width / 2.0f, backBounds.top + backBounds.height / 2.0f);
 		
 		
-		test = new RectangleShape();
-		test.setPosition(displayedScore.getPosition());
-		//test.setPosition(Vector2f.add(displayedScore.getPosition(), new Vector2f(displayedScore.getLocalBounds().width, displayedScore.getLocalBounds().height)));
-		test.setSize(new Vector2f(displayedScore.getLocalBounds().width, displayedScore.getLocalBounds().height));
-		test.setFillColor(Color.BLACK);
+		rightScoreBackground.setPosition(rightPositioning);
+		rightDisplayedScore.setPosition(rightPositioning);
+		
+		leftScoreBackground = rightScoreBackground;
+		leftDisplayedScore = rightDisplayedScore;
+		
+		leftScoreBackground = rightScoreBackground;
+		leftScoreBackground.setPosition(rightPositioning);
+		
+		
+		
+		
+		
 	}
 
 	public void draw(RenderTarget target, RenderStates states) {
 		
-		scoreBackground.draw(target, states);
-		displayedScore.draw(target, states);
-		test.draw(target, states);
+		rightScoreBackground.draw(target, states);
+		rightDisplayedScore.draw(target, states);
+		
+		leftScoreBackground.draw(target, states);
+		leftDisplayedScore.draw(target, states);
 	}
 
 }
